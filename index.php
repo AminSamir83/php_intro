68ac5ab6f8f20c611b925d5209938b0c7a3570de
AminSamir83 <37070162+AminSamir83@users.noreply.github.com>
2018-11-07 10:23:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php echo "Ceci est une ligne créée uniquement en PHP";
+    echo "<br>";
+echo "Ceci est la 2ème phrase créée avec PHP. <br>";
+echo "<a href='https://gomycode.tn/'> link </a>" ?>;
